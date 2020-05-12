f69bc0536a15fc181a12d8f147c56af074b65faa
Thais Bastos <thais.bastos@simpli.com.br>
2020-05-12 17:24:07 -0300

    Command neo3-boa

--- setup.py
+++ setup.py
@@ -97,9 +97,9 @@ setup(
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    # entry_points={
-    #     'console_scripts': [
-    #         'neo3-boa=boa.cli:main',
-    #     ],
-    # },
+    entry_points={
+       'console_scripts': [
+           'neo3-boa=boa3.cli:main',
+       ],
+    },
 )
