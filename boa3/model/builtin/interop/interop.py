0fc9be12f297429e32fb47f7c5061c630e140d6e
luc10921 <kenjiuezu3@gmail.com>
2020-11-16 17:52:15 -0300

    Boa-191 Implement Destroy Contract Interop

--- boa3/model/builtin/interop/interop.py
+++ boa3/model/builtin/interop/interop.py
@@ -12,7 +12,6 @@ from boa3.model.builtin.interop.contract.updatemethod import UpdateMethod
 from boa3.model.builtin.interop.contract.destroymethod import DestroyMethod
 from boa3.model.builtin.interop.contract.getgasscripthashmethod import GasProperty
 from boa3.model.builtin.interop.contract.getneoscripthashmethod import NeoProperty
-from boa3.model.builtin.interop.contract.updatemethod import UpdateMethod
 from boa3.model.builtin.interop.crypto.checkmultisigwithecdsasecp256k1method import (
     CheckMultisigWithECDsaSecp256k1Method)
 from boa3.model.builtin.interop.crypto.checkmultisigwithecdsasecp256r1method import (
