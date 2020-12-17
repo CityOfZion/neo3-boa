4170857ea73104a36db982a2a1d0a3e2a1cfd538
Mirella de Medeiros <mirellamedeiros.09@hotmail.com>
2020-12-17 19:59:29 -0300

    Add values to storage to create test balance balues

---
+++ boa3_test/tests/test_classes/nativeaccountstate.py
@@ -0,0 +1,25 @@
+from typing import Any
+
+from boa3.neo.vm.type import StackItem
+from boa3.neo.vm.type.StackItem import StackItemType
+
+
+class NativeAccountState:
+    def __init__(self, balance: int):
+        self.balance: int = balance if balance > 0 else 0
+        self._balance_height: int = 0
+        self._vote_to: Any = None
+
+    def serialize(self) -> bytes:
+        # TODO: change this when Struct is implemented
+        return (StackItemType.Struct
+                + b'\x03'
+                + StackItem.serialize(self.balance)
+                + b'!\x00\x00'
+                )
+
+    # @classmethod
+    # def deserialize(cls, data: bytes):
+    #     result = StackItem.
+
+
