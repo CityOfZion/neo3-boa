dc903100816a0be4fd4ee09188c127b896db4b46
Mirella de Medeiros <mirellamedeiros.09@hotmail.com>
2021-03-04 17:26:20 -0300

    Fix return when using update_contract and destroy_contract

--- boa3_test/test_sc/interop_test/UpdateContract.py
+++ boa3_test/test_sc/interop_test/UpdateContract.py
@@ -5,3 +5,8 @@ from boa3.builtin.interop.contract import update_contract
 @public
 def update(script: bytes, manifest: bytes):
     update_contract(script, manifest)
+
+
+@public
+def new_method() -> int:
+    return 42
