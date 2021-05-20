32c2ee86adbdc5c00e20bc8ec15bedd65dbc51f9
Mirella de Medeiros <mirellamedeiros.09@hotmail.com>
2021-05-20 11:01:58 -0300

    Update debug info file generation

---
+++ boa3_test/test_sc/generation_test/GenerationWithStaticVariables.py
@@ -0,0 +1,15 @@
+from boa3.builtin import public
+
+
+var1 = 42
+var2 = '42'
+
+
+@public
+def Add(a: int, b: int) -> int:
+    return a + b
+
+
+@public
+def Sub(a: int, b: int) -> int:
+    return a - b
