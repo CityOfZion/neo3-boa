7082b044a478a65939900e45e98d65a8807082f8
luc10921 <lucas.uezu@simpli.com.br>
2021-05-24 14:44:54 -0300

    Add a new signature to update_contract() interop

--- boa3_test/test_sc/interop_test/contract/UpdateContract.py
+++ boa3_test/test_sc/interop_test/contract/UpdateContract.py
@@ -1,7 +1,9 @@
+from typing import Any
+
 from boa3.builtin import public
 from boa3.builtin.interop.contract import update_contract
 
 
 @public
-def update(script: bytes, manifest: bytes):
-    update_contract(script, manifest)
+def update(script: bytes, manifest: bytes, data: Any):
+    update_contract(script, manifest, data)
