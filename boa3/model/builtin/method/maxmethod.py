a9e069b22fcb4213e7c5115f2c55d079d34c372c
luc10921 <lucas.uezu@simpli.com.br>
2021-07-01 16:55:14 -0300

    added logic to get at least 2 int arguments

--- boa3/model/builtin/method/maxmethod.py
+++ boa3/model/builtin/method/maxmethod.py
@@ -14,11 +14,14 @@ class MaxMethod(IBuiltinMethod):
     def __init__(self, arg_value: Optional[IType] = None):
         from boa3.model.type.type import Type
         identifier = 'max'
-        allowed_types = Type.union.build([Type.int])
+        allowed_types = Type.int
         if not isinstance(arg_value, IType):
             arg_value = allowed_types
 
-        args: Dict[str, Variable] = {}
+        args: Dict[str, Variable] = {
+            'args1': Variable(Type.int),
+            'args2': Variable(Type.int)
+        }
         vararg = ('values', Variable(arg_value))
         super().__init__(identifier, args, return_type=arg_value, vararg=vararg)
         self._allowed_types = allowed_types
@@ -39,12 +42,38 @@ class MaxMethod(IBuiltinMethod):
         from boa3.compiler.codegenerator import get_bytes_count
         from boa3.neo.vm.type.Integer import Integer
 
-        recreate_array = [  # recreates the array to not change the referenced parameter
-            (Opcode.UNPACK, b''),
-            (Opcode.PACK, b'')
+        jmp_place_holder = (Opcode.JMP, b'\x01')
+
+        verify_number_of_parameters = [     # verifies if the stack has 2 or 3 items
+            (Opcode.DEPTH, b''),
+            (Opcode.PUSH2, b''),
+            (Opcode.JMPEQ, b''),
+            jmp_place_holder
         ]
 
-        if_int_initialize = [   # puts the last array element as the max value
+        if_n_parameters_gt_2 = [         # if number of items in stack is 3 (2 ints and one tuple)
+            (Opcode.REVERSE3, b''),
+            (Opcode.UNPACK, b''),
+            (Opcode.INC, b''),
+            (Opcode.INC, b''),
+            jmp_place_holder    # skips the next block of instructions
+        ]
+
+        if_n_parameters_eq_2 = [        # if number of items in stack is 2 (2 ints)
+            (Opcode.PUSH2, b'')
+        ]
+
+        jmp_n_parameters_eq_2 = Opcode.get_jump_and_data(Opcode.JMP, get_bytes_count(if_n_parameters_eq_2), True)
+        if_n_parameters_gt_2[-1] = jmp_n_parameters_eq_2
+
+        jmp_n_parameters_gt_2 = Opcode.get_jump_and_data(Opcode.JMPEQ, get_bytes_count(if_n_parameters_gt_2))
+        verify_number_of_parameters[-1] = jmp_n_parameters_gt_2
+
+        repack_array = [        # pack all the arguments in the array
+            (Opcode.PACK, b''),
+        ]
+
+        is_int_initialize = [   # puts the last array element as the max value
             (Opcode.DUP, b''),          # index = len(array) - 1
             (Opcode.SIZE, b''),
             (Opcode.DEC, b''),
@@ -53,7 +82,7 @@ class MaxMethod(IBuiltinMethod):
             (Opcode.PICKITEM, b''),     # max = array[index]
         ]
 
-        if_int_while = [    # this will get the next number in the array and compare it with the current max
+        is_int_while = [    # this will get the next number in the array and compare it with the current max
             (Opcode.SWAP, b''),         # index--
             (Opcode.DEC, b''),
             (Opcode.SWAP, b''),
@@ -69,8 +98,8 @@ class MaxMethod(IBuiltinMethod):
             # else go to the end
         ]
 
-        jmp_back_to_while_statement = (Opcode.JMPIF, Integer(-get_bytes_count(if_int_while)).to_byte_array(signed=True))
-        if_int_while.append(jmp_back_to_while_statement)
+        jmp_back_to_while_statement = (Opcode.JMPIF, Integer(-get_bytes_count(is_int_while)).to_byte_array(signed=True))
+        is_int_while.append(jmp_back_to_while_statement)
 
         clean_stack = [    # removes everything but max
             (Opcode.REVERSE3, b''),
@@ -79,9 +108,12 @@ class MaxMethod(IBuiltinMethod):
         ]
 
         return (
-            recreate_array +
-            if_int_initialize +
-            if_int_while +
+            verify_number_of_parameters +
+            if_n_parameters_gt_2 +
+            if_n_parameters_eq_2 +
+            repack_array +
+            is_int_initialize +
+            is_int_while +
             clean_stack
         )
 
@@ -94,7 +126,7 @@ class MaxMethod(IBuiltinMethod):
         return
 
     def build(self, value: Any) -> IBuiltinMethod:
-        if isinstance(value, list) and len(value) == 1:
+        if isinstance(value, list) and len(value) > 0:
             value = value[0]
         if isinstance(value, TupleType):
             value = value.value_type
