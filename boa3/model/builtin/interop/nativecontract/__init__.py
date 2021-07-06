1f3c993c9e0d5b895a44caeea2b66b1c13250060
luc10921 <lucas.uezu@simpli.com.br>
2021-07-06 16:21:14 -0300

    implemented PolicyContract

--- boa3/model/builtin/interop/nativecontract/__init__.py
+++ boa3/model/builtin/interop/nativecontract/__init__.py
@@ -2,5 +2,6 @@ from .ContractManagement import ContractManagement, ContractManagementMethod
 from .CryptoLib import CryptoLibContract, CryptoLibMethod
 from .Ledger import LedgerContract, LedgerMethod
 from .Oracle import OracleContract, OracleMethod
+from .PolicyContract import PolicyContract, PolicyContractMethod
 from .RoleManagement import RoleManagement, RoleManagementMethod
 from .StdLib import StdLibContract, StdLibMethod
