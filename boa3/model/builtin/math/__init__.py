fe26132b60d0e62f0375e40728a62d4e187c9151
Mirella de Medeiros <mirellamedeiros.09@hotmail.com>
2022-01-07 12:15:41 -0300

    Add support to ceil

--- boa3/model/builtin/math/__init__.py
+++ boa3/model/builtin/math/__init__.py
@@ -1,6 +1,8 @@
-__all__ = ['DecimalFloorMethod',
+__all__ = ['DecimalCeilingMethod',
+           'DecimalFloorMethod',
            'SqrtMethod',
            ]
 
+from boa3.model.builtin.math.decimalceil import DecimalCeilingMethod
 from boa3.model.builtin.math.decimalfloor import DecimalFloorMethod
 from boa3.model.builtin.math.sqrtmethod import SqrtMethod
