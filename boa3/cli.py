2531423bd0a9d913316206f418662af0a845bd47
luc10921 <lucas.uezu@simpli.com.br>
2022-01-31 12:09:46 -0300

    Change nef debug file generation to be optional

--- boa3/cli.py
+++ boa3/cli.py
@@ -10,6 +10,7 @@ from boa3.exception.NotLoadedException import NotLoadedException
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help=".py smart contract to compile")
+    parser.add_argument("-db", "--debug", action='store_true', help="generates a nefdbgnfo file")
     args = parser.parse_args()
 
     if not args.input.endswith(".py") or not os.path.isfile(args.input):
@@ -20,7 +21,7 @@ def main():
     path, filename = os.path.split(fullpath)
 
     try:
-        Boa3.compile_and_save(args.input)
+        Boa3.compile_and_save(args.input, args.debug)
         logging.info(f"Wrote {filename.replace('.py', '.nef')} to {path}")
     except NotLoadedException as e:
         logging.error("Could not compile")
