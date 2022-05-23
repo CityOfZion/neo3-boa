a1beecdc16f4100e35a138ec127b6cd163e5d526
Mirella de Medeiros <mirellamedeiros.09@hotmail.com>
2022-05-23 12:26:19 -0300

    #29h3y3p Unresolved reference 'Event'

--- boa3/exception/NotLoadedException.py
+++ boa3/exception/NotLoadedException.py
@@ -1,2 +1,11 @@
 class NotLoadedException(Exception):
-    pass
+    def __init__(self, empty_script: bool = False):
+        self.empty_script = empty_script
+
+    @property
+    def message(self) -> str:
+        exception_message = ''
+        if self.empty_script:
+            exception_message = 'An empty script was generated'
+
+        return exception_message
