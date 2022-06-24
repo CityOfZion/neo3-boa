d29c8c668c9c8b2b01cfa769f1d69c3808ed4873
luc10921 <lucas.uezu@simpli.com.br>
2022-06-24 13:59:52 -0300

    Method call is falling when calling other methods as arguments | #2m2fbdz

---
+++ boa3_test/test_sc/function_test/FunctionWithArgAsArg.py
@@ -0,0 +1,14 @@
+from typing import List
+
+from boa3.builtin import public
+
+
+@public
+def main(a: List[int], value: int) -> int:
+    var = return_same(a.index(value))
+
+    return var
+
+
+def return_same(arg: int) -> int:
+    return arg
