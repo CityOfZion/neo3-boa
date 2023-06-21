e762bcc902c3a99d1847ff2ed7f5947884774b2b
luc10921 <lucas.uezu@simpli.com.br>
2023-06-21 11:39:39 -0300

    CU-864eq2qk4 - Refactor sum implementation to improve readability

--- boa3/internal/model/builtin/method/summethod.py
+++ boa3/internal/model/builtin/method/summethod.py
@@ -1,5 +1,5 @@
 import ast
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, Optional
 
 from boa3.internal.model.builtin.method.builtinmethod import IBuiltinMethod
 from boa3.internal.model.variable import Variable
@@ -18,29 +18,37 @@ class SumMethod(IBuiltinMethod):
                                   ).body[0].value
         super().__init__(identifier, args, defaults=[start_default], return_type=Type.int)
 
-    @property
-    def _opcode(self) -> List[Tuple[Opcode, bytes]]:
-        from boa3.internal.neo.vm.type.Integer import Integer
-        return [
-            (Opcode.PUSH0, b''),    # index = 0
-            (Opcode.DUP, b''),
-            (Opcode.PUSH2, b''),    # len(iterable)
-            (Opcode.PICK, b''),
-            (Opcode.SIZE, b''),
-            (Opcode.GE, b''),       # index > len(iterable)
-            (Opcode.JMPIF, Integer(12).to_byte_array(signed=True, min_length=1)),
-            (Opcode.REVERSE3, b''),
-            (Opcode.OVER, b''),
-            (Opcode.PUSH3, b''),    # x = iterable[index]
-            (Opcode.PICK, b''),
-            (Opcode.PICKITEM, b''),
-            (Opcode.ADD, b''),      # result += x
-            (Opcode.REVERSE3, b''),
-            (Opcode.INC, b''),      # index += 1
-            (Opcode.JMP, Integer(-15).to_byte_array(signed=True, min_length=1)),
-            (Opcode.DROP, b''),
-            (Opcode.DROP, b''),
-        ]
+    def generate_internal_opcodes(self, code_generator):
+        from boa3.internal.model.builtin.builtin import Builtin
+        from boa3.internal.model.operation.binaryop import BinaryOp
+
+        # index = 0
+        code_generator.convert_literal(0)
+
+        # while index < len(iterable)
+        while_start = code_generator.convert_begin_while()
+
+        code_generator.swap_reverse_stack_items(3)
+        code_generator.duplicate_stack_item(2)
+        code_generator.duplicate_stack_item(4)
+        code_generator.convert_get_item(index_inserted_internally=True, test_is_negative_index=False)
+        # sum += iterable[index]
+        code_generator.convert_operation(BinaryOp.Add, is_internal=True)
+        code_generator.swap_reverse_stack_items(3)
+        # index += 1
+        code_generator.insert_opcode(Opcode.INC)
+
+        # while condition and end
+        while_condition = code_generator.bytecode_size
+        code_generator.duplicate_stack_top_item()
+        code_generator.duplicate_stack_item(3)
+        code_generator.convert_builtin_method_call(Builtin.Len, is_internal=True)
+        code_generator.convert_operation(BinaryOp.Lt, is_internal=True)
+        code_generator.convert_end_while(while_start, while_condition, is_internal=True)
+
+        # remove auxiliary values from stack
+        code_generator.remove_stack_top_item()
+        code_generator.remove_stack_top_item()
 
     @property
     def _args_on_stack(self) -> int:
