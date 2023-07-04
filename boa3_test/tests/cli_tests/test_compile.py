39b91ea0fc6578ccce0d908ca67decfc31022de6
Mirella de Medeiros <mirellamedeiros.09@hotmail.com>
2023-07-04 17:45:35 -0300

    #864e9nnrp - get unique names for compiled files on cli tests

--- boa3_test/tests/cli_tests/test_compile.py
+++ boa3_test/tests/cli_tests/test_compile.py
@@ -1,27 +1,18 @@
-from boa3_test.tests.boa_test import BoaTest, _COMPILER_LOCK as LOCK, _LOGGING_LOCK as LOG_LOCK  # needs to be the first import to avoid circular imports
+from boa3_test.tests.cli_tests.cli_test import BoaCliTest  # needs to be the first import to avoid circular imports
 
-import io
-import os
-from boa3.cli import main
+import os.path
+
 from boa3.internal import constants
 from boa3.internal.neo3.vm import VMState
-from boa3_test.tests.cli_tests.utils import neo3_boa_cli, get_path_from_boa3_test, normalize_separators
 from boa3_test.test_drive.testrunner.neo_test_runner import NeoTestRunner
-from contextlib import redirect_stdout
+from boa3_test.tests.cli_tests.utils import neo3_boa_cli, get_path_from_boa3_test
 
 
-class TestCliCompile(BoaTest):
-    default_folder = 'test_cli'
-
-    EXIT_CODE_SUCCESS = 0
-    EXIT_CODE_ERROR = 1
-    EXIT_CODE_CLI_SYNTAX_ERROR = 2
+class TestCliCompile(BoaCliTest):
 
     @neo3_boa_cli('compile', '-h')
     def test_cli_compile_help(self):
-        with LOCK, LOG_LOCK, redirect_stdout(io.StringIO()) as output, self.assertRaises(SystemExit) as system_exit:
-            main()
-        cli_output = normalize_separators(output.getvalue())
+        cli_output, _, system_exit = self._assert_cli_raises(SystemExit)
 
         self.assertEqual(self.EXIT_CODE_SUCCESS, system_exit.exception.code)
         self.assertIn('usage: neo3-boa compile [-h] [-db] [--project-path PROJECT_PATH] [-e ENV] [-o NEF_OUTPUT] input',
@@ -41,8 +32,7 @@ class TestCliCompile(BoaTest):
         if os.path.isfile(debug_info_path):
             os.remove(debug_info_path)
 
-        with LOCK, LOG_LOCK, self.assertLogs() as logs:
-            main()
+        logs = self._get_cli_log()
 
         self.assertTrue(any(f'neo3-boa v{constants.BOA_VERSION}\tPython {constants.SYS_VERSION}' in log for log in logs.output))
         self.assertTrue(any('Started compiling' in log for log in logs.output))
@@ -66,8 +56,7 @@ class TestCliCompile(BoaTest):
         if os.path.isfile(debug_info_path):
             os.remove(debug_info_path)
 
-        with LOCK, LOG_LOCK, self.assertLogs() as logs:
-            main()
+        logs = self._get_cli_log()
 
         self.assertTrue(any(f'neo3-boa v{constants.BOA_VERSION}\tPython {constants.SYS_VERSION}' in log for log in logs.output))
         self.assertTrue(any('Started compiling' in log for log in logs.output))
@@ -77,19 +66,23 @@ class TestCliCompile(BoaTest):
         self.assertTrue(os.path.isfile(debug_info_path))
 
     @neo3_boa_cli('compile', get_path_from_boa3_test('test_sc', 'boa_built_in_methods_test', 'Env.py'),
-                  '-o', get_path_from_boa3_test('test_cli', 'smart_contract.nef'))
+                  '-o', get_path_from_boa3_test('test_cli', 'smart_contract.nef', get_unique=True))
     def test_cli_compile_new_output_path(self):
         sc_nef_name = 'smart_contract.nef'
-        nef_path = get_path_from_boa3_test('test_cli', sc_nef_name)
-        manifest_path = nef_path.replace('nef', 'manifest.json')
+
+        nef_path, manifest_path = self.get_deploy_file_paths(
+            get_path_from_boa3_test('test_sc', 'boa_built_in_methods_test', 'Env.py'),
+            output_name=get_path_from_boa3_test('test_cli', sc_nef_name),
+            compile_if_found=False
+        )
+        sc_nef_name = os.path.basename(nef_path)
 
         if os.path.isfile(nef_path):
             os.remove(nef_path)
         if os.path.isfile(manifest_path):
             os.remove(manifest_path)
 
-        with LOCK, LOG_LOCK, self.assertLogs() as logs:
-            main()
+        logs = self._get_cli_log()
 
         self.assertTrue(any(f'neo3-boa v{constants.BOA_VERSION}\tPython {constants.SYS_VERSION}' in log for log in logs.output))
         self.assertTrue(any('Started compiling' in log for log in logs.output))
@@ -98,21 +91,25 @@ class TestCliCompile(BoaTest):
         self.assertTrue(os.path.isfile(manifest_path))
 
     @neo3_boa_cli('compile', get_path_from_boa3_test('test_sc', 'boa_built_in_methods_test', 'Env.py'),
-                  '-e', 'env_changed')
+                  '-e', 'env_changed',
+                  '-o', get_path_from_boa3_test('test_sc', 'boa_built_in_methods_test', 'Env.nef', get_unique=True))
     def test_cli_compile_env(self):
-        sc_nef_name = 'Env.nef'
+        nef_path, _ = self.get_deploy_file_paths(
+            get_path_from_boa3_test('test_sc', 'boa_built_in_methods_test', 'Env.py'),
+            compile_if_found=False
+        )
+        nef_generated = nef_path.split(constants.PATH_SEPARATOR)[-1]
 
-        with LOCK, LOG_LOCK, self.assertLogs() as logs:
-            main()
+        logs = self._get_cli_log()
+
         self.assertTrue(any(f'neo3-boa v{constants.BOA_VERSION}\tPython {constants.SYS_VERSION}' in log for log in logs.output))
         self.assertTrue(any('Started compiling' in log for log in logs.output))
-        self.assertTrue(any(f'Wrote {sc_nef_name} to ' in log in log for log in logs.output))
+        self.assertTrue(any(f'Wrote {nef_generated} to ' in log in log for log in logs.output))
 
-        path = get_path_from_boa3_test('test_sc', 'boa_built_in_methods_test', sc_nef_name)
         runner = NeoTestRunner(runner_id=self.method_name())
 
-        runner.deploy_contract(path)
-        invoke = runner.call_contract(path, 'main')
+        runner.deploy_contract(nef_path)
+        invoke = runner.call_contract(nef_path, 'main')
         expected_result = 'env_changed'
         runner.execute()
         self.assertEqual(VMState.HALT, runner.vm_state, msg=runner.error)
@@ -121,24 +118,20 @@ class TestCliCompile(BoaTest):
 
     @neo3_boa_cli('compile', 'wrong_file')
     def test_cli_compile_wrong_file(self):
-        with LOCK, LOG_LOCK, self.assertLogs() as logs, self.assertRaises(SystemExit) as system_exit:
-            main()
+        logs, system_exit = self._assert_cli_raises(SystemExit, get_log=True)
 
         self.assertEqual(self.EXIT_CODE_ERROR, system_exit.exception.code)
         self.assertTrue(any('Input file is not .py' in log in log for log in logs.output))
 
     @neo3_boa_cli('compile', get_path_from_boa3_test('test_sc', 'interop_test', 'storage', 'StoragePutStrKeyStrValue.py'))
     def test_cli_compile_invalid_smart_contract(self):
-        with LOCK, LOG_LOCK, self.assertLogs() as logs:
-            main()
-
+        logs = self._get_cli_log()
         self.assertTrue(any('Could not compile' in log in log for log in logs.output))
 
     @neo3_boa_cli('compile', get_path_from_boa3_test('test_sc', 'boa_built_in_methods_test', 'Env.py'),
                   '-o', 'wrong_output_path')
     def test_cli_compile_wrong_output_path(self):
-        with LOCK, LOG_LOCK, self.assertLogs() as logs, self.assertRaises(SystemExit) as system_exit:
-            main()
+        logs, system_exit = self._assert_cli_raises(SystemExit, get_log=True)
 
         self.assertEqual(self.EXIT_CODE_ERROR, system_exit.exception.code)
         self.assertTrue(any('Output path file extension is not .nef' in log in log for log in logs.output))
--- boa3_test/tests/cli_tests/utils.py
+++ boa3_test/tests/cli_tests/utils.py
@@ -1,13 +1,26 @@
-from boa3.internal import constants, env
 from unittest.mock import patch
+
+from boa3.internal import constants, env
 
 
 def neo3_boa_cli(*args: str):
     return patch('sys.argv', ['neo3-boa', *args])
 
 
-def get_path_from_boa3_test(*args: str) -> str:
-    return constants.PATH_SEPARATOR.join([env.PROJECT_ROOT_DIRECTORY, 'boa3_test', *args])
+def get_path_from_boa3_test(*args: str, get_unique=False) -> str:
+    result = constants.PATH_SEPARATOR.join([env.PROJECT_ROOT_DIRECTORY, 'boa3_test', *args])
+
+    if get_unique:
+        from boa3_test.tests.boa_test import USE_UNIQUE_NAME
+
+        if USE_UNIQUE_NAME:
+            import os.path
+            from boa3_test.test_drive import utils
+
+            file_path_without_ext, ext = os.path.splitext(result)
+            file_path_without_ext = utils.create_custom_id(file_path_without_ext, use_time=False)
+            result = file_path_without_ext + ext
+    return result
 
 
 def normalize_separators(original_string: str) -> str:
