333b0b636f0dfdd20235019337b00159f4672e39
Mirella de Medeiros <mirellamedeiros.09@hotmail.com>
2023-07-04 17:45:35 -0300

    #864e9nnrp - get unique names for compiled files on cli tests

--- boa3_test/tests/cli_tests/test_cli.py
+++ boa3_test/tests/cli_tests/test_cli.py
@@ -1,24 +1,14 @@
-from boa3_test.tests.boa_test import BoaTest, _COMPILER_LOCK as LOCK, _LOGGING_LOCK as LOG_LOCK  # needs to be the first import to avoid circular imports
+from boa3_test.tests.cli_tests.cli_test import BoaCliTest  # needs to be the first import to avoid circular imports
 
-import io
-from boa3.cli import main
 from boa3.internal import constants
-from boa3_test.tests.cli_tests.utils import neo3_boa_cli, normalize_separators
-from contextlib import redirect_stdout, redirect_stderr
+from boa3_test.tests.cli_tests.utils import neo3_boa_cli
 
 
-class TestCli(BoaTest):
-    default_folder = 'test_cli'
-
-    EXIT_CODE_SUCCESS = 0
-    EXIT_CODE_ERROR = 1
-    EXIT_CODE_CLI_SYNTAX_ERROR = 2
+class TestCli(BoaCliTest):
 
     @neo3_boa_cli('-h')
     def test_cli_help(self):
-        with LOCK, LOG_LOCK, redirect_stdout(io.StringIO()) as output, self.assertRaises(SystemExit) as system_exit:
-            main()
-        cli_output = normalize_separators(output.getvalue())
+        cli_output, _, system_exit = self._assert_cli_raises(SystemExit)
 
         self.assertEqual(self.EXIT_CODE_SUCCESS, system_exit.exception.code)
         self.assertIn('usage: neo3-boa [-h] [-v] {compile}', cli_output)
@@ -27,18 +17,14 @@ class TestCli(BoaTest):
 
     @neo3_boa_cli('--version')
     def test_cli_version(self):
-        with LOCK, LOG_LOCK, redirect_stdout(io.StringIO()) as output, self.assertRaises(SystemExit) as system_exit:
-            main()
-        cli_output = normalize_separators(output.getvalue())
+        cli_output, _, system_exit = self._assert_cli_raises(SystemExit)
 
         self.assertEqual(self.EXIT_CODE_SUCCESS, system_exit.exception.code)
         self.assertIn(f'neo3-boa {constants.BOA_VERSION}', cli_output)
 
     @neo3_boa_cli('build')
     def test_cli_wrong_syntax(self):
-        with LOCK, LOG_LOCK, redirect_stderr(io.StringIO()) as output, self.assertRaises(SystemExit) as system_exit:
-            main()
-        cli_output = normalize_separators(output.getvalue())
+        _, cli_output, system_exit = self._assert_cli_raises(SystemExit)
 
         self.assertEqual(self.EXIT_CODE_CLI_SYNTAX_ERROR, system_exit.exception.code)
         self.assertIn("invalid choice: 'build'", cli_output)
