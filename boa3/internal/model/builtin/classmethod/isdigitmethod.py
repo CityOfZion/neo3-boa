11008baef1852205fd450e216f444766e6b755ec
luc10921 <lucas.uezu@simpli.com.br>
2023-07-21 11:29:50 -0300

    CU-864eq2u7w - Refactor isdigit implementation to improve readability

--- boa3/internal/model/builtin/classmethod/isdigitmethod.py
+++ boa3/internal/model/builtin/classmethod/isdigitmethod.py
@@ -23,122 +23,73 @@ class IsDigitMethod(IBuiltinMethod):
     def _arg_self(self) -> Variable:
         return self.args['self']
 
-    @property
-    def _opcode(self) -> List[Tuple[Opcode, bytes]]:
-        from boa3.internal.compiler.codegenerator import get_bytes_count
-        from boa3.internal.neo.vm.type.Integer import Integer
-        from boa3.internal.neo.vm.type.StackItem import StackItemType
+    def generate_internal_opcodes(self, code_generator):
+        from boa3.internal.model.builtin.builtin import Builtin
+        from boa3.internal.model.operation.binaryop import BinaryOp
 
-        number0 = Integer(ord('0')).to_byte_array()
-        number9 = Integer(ord('9')).to_byte_array()
-        jmp_place_holder = (Opcode.JMP, b'\x01')
+        # index = len(string) - 1
+        code_generator.duplicate_stack_top_item()
+        code_generator.convert_builtin_method_call(Builtin.Len, is_internal=True)
+        code_generator.insert_opcode(Opcode.DEC)
+        # isdigit = True
+        code_generator.convert_literal(True)
 
-        initializing = [                    # initialize auxiliary values
-            (Opcode.DUP, b''),
-            (Opcode.SIZE, b''),
-            (Opcode.DEC, b''),              # index = len(string) - 1
-            (Opcode.PUSH1, b''),            # isdigit = True
-        ]
+        # if len(string) > 0:
+        code_generator.duplicate_stack_item(2)
+        code_generator.convert_literal(-1)
+        if_string_not_empty = code_generator.convert_begin_if()
+        code_generator.change_jump(if_string_not_empty, Opcode.JMPEQ)
 
-        verify_empty_string = [             # verifies if string is empty
-            (Opcode.OVER, b''),
-            (Opcode.PUSHM1, b''),
-            jmp_place_holder,               # jump to change_to_false if index == -1
-        ]
+        #   while index >= 0:
+        while_str_is_analysed = code_generator.convert_begin_while()
+        code_generator.duplicate_stack_item(3)
+        code_generator.duplicate_stack_item(3)
+        code_generator.convert_literal(1)
+        code_generator.convert_get_substring(is_internal=True)
+        code_generator.duplicate_stack_top_item()
+        code_generator.convert_literal('0')
 
-        skip_first_verify_while = [         # skips the first while verification, since string is not empty
-            jmp_place_holder
-        ]
+        #       if ord(string[index]) < ord('0'):
+        if_char_lt_0 = code_generator.convert_begin_if()
+        code_generator.change_jump(if_char_lt_0, Opcode.JMPGE)
+        code_generator.remove_stack_top_item()
+        code_generator.remove_stack_top_item()
+        #           return False
+        code_generator.convert_literal(False)
+        code_generator.convert_loop_break()
+        code_generator.convert_end_if(if_char_lt_0, is_internal=True)
 
-        verify_while = [                    # verifies if while is over
-            (Opcode.OVER, b''),
-            (Opcode.PUSH0, b''),
-            jmp_place_holder,               # jump to return_bool if index >= len(string)
-        ]
+        #       elif ord(string[index]) > ord('9'):
+        code_generator.convert_literal('9')
+        if_char_gt_9 = code_generator.convert_begin_if()
+        code_generator.change_jump(if_char_gt_9, Opcode.JMPLE)
+        #           return False
+        code_generator.remove_stack_top_item()
+        code_generator.convert_literal(False)
+        code_generator.convert_loop_break()
+        code_generator.convert_end_if(if_char_gt_9, is_internal=True)
 
-        jmp_verify_while = OpcodeHelper.get_jump_and_data(Opcode.JMP, get_bytes_count(verify_while), True)
-        skip_first_verify_while[-1] = jmp_verify_while
+        #       index -= 1
+        code_generator.swap_reverse_stack_items(2)
+        code_generator.insert_opcode(Opcode.DEC)
+        code_generator.swap_reverse_stack_items(2)
 
-        while_verify_lt_0 = [               # verifies if ord(string[index]) is < ord('0')
-            (Opcode.PUSH2, b''),
-            (Opcode.PICK, b''),
-            (Opcode.PUSH2, b''),
-            (Opcode.PICK, b''),
-            (Opcode.PUSH1, b''),
-            (Opcode.SUBSTR, b''),
-            (Opcode.CONVERT, StackItemType.ByteString),
-            (Opcode.DUP, b''),
-            (Opcode.PUSHDATA1, Integer(len(number0)).to_byte_array() + number0),
-            jmp_place_holder,               # if ord(string[index]) < ord('0'), return False
-        ]
+        # verify if already passed through the whole string
+        while_condition = code_generator.bytecode_size
+        code_generator.duplicate_stack_item(2)
+        code_generator.convert_literal(0)
+        code_generator.convert_operation(BinaryOp.GtE, is_internal=True)
+        code_generator.convert_end_while(while_str_is_analysed, while_condition, is_internal=True)
 
-        while_verify_gt_9 = [               # verifies if ord(string[index]) is > ord('9')
-            (Opcode.PUSHDATA1, Integer(len(number9)).to_byte_array() + number9),
-            jmp_place_holder,               # if ord(string[index]) > ord('9'), return False
-        ]
+        # elif len(string) == 0:
+        else_string_is_empty = code_generator.convert_begin_else(if_string_not_empty, is_internal=True)
+        code_generator.remove_stack_top_item()
+        #   return False
+        code_generator.convert_literal(False)
+        code_generator.convert_end_if(else_string_is_empty, is_internal=True)
 
-        while_go_to_verify = [              # decreases index and goes back to verify if there all characters were visited already
-            (Opcode.SWAP, b''),
-            (Opcode.DEC, b''),              # index--
-            (Opcode.SWAP, b''),
-            # jump back to verify_while
-        ]
-
-        jmp_back_to_verify = OpcodeHelper.get_jump_and_data(Opcode.JMP, -get_bytes_count(verify_while +
-                                                                                         while_verify_lt_0 +
-                                                                                         while_verify_gt_9 +
-                                                                                         while_go_to_verify))
-        while_go_to_verify.append(jmp_back_to_verify)
-
-        jmp_out_of_while = OpcodeHelper.get_jump_and_data(Opcode.JMPLT, get_bytes_count(while_verify_gt_9 +
-                                                                                        while_go_to_verify), True)
-        while_verify_lt_0[-1] = jmp_out_of_while
-
-        drop_char = [                       # remove extra char from stack
-            (Opcode.DROP, b'')
-        ]
-
-        jmp_to_change_to_false = OpcodeHelper.get_jump_and_data(Opcode.JMPGT, get_bytes_count(while_go_to_verify +
-                                                                                              drop_char), True)
-        while_verify_gt_9[-1] = jmp_to_change_to_false
-
-        change_to_false = [                 # remove True on top of stack and put False
-            (Opcode.DROP, b''),
-            (Opcode.PUSH0, b''),
-        ]
-
-        jmp_to_return = OpcodeHelper.get_jump_and_data(Opcode.JMPEQ, get_bytes_count(skip_first_verify_while +
-                                                                                     verify_while +
-                                                                                     while_verify_lt_0 +
-                                                                                     while_verify_gt_9 +
-                                                                                     while_go_to_verify +
-                                                                                     drop_char), True)
-        verify_empty_string[-1] = jmp_to_return
-
-        jmp_to_return = OpcodeHelper.get_jump_and_data(Opcode.JMPLT, get_bytes_count(while_verify_lt_0 +
-                                                                                     while_verify_gt_9 +
-                                                                                     while_go_to_verify +
-                                                                                     drop_char +
-                                                                                     change_to_false), True)
-        verify_while[-1] = jmp_to_return
-
-        clean_and_return_bool = [           # remove extra values from stack
-            (Opcode.NIP, b''),
-            (Opcode.NIP, b'')
-        ]
-
-        return (
-            initializing +
-            verify_empty_string +
-            skip_first_verify_while +
-            verify_while +
-            while_verify_lt_0 +
-            while_verify_gt_9 +
-            while_go_to_verify +
-            drop_char +
-            change_to_false +
-            clean_and_return_bool
-        )
+        code_generator.remove_stack_item(2)
+        code_generator.remove_stack_item(2)
 
     def push_self_first(self) -> bool:
         return self.has_self_argument
