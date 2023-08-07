b6d0e400471a9f26511d47cbd7a1dacb3e4a4914
Mirella de Medeiros <mirella.medeiros@simpli.com.br>
2023-08-07 14:49:13 -0300

    #864eu13yg - Fix unit tests failures on staging

--- boa3/internal/compiler/codegenerator/codegenerator.py
+++ boa3/internal/compiler/codegenerator/codegenerator.py
@@ -1731,23 +1731,10 @@ class CodeGenerator:
         elif previous_stack_size < 0:
             previous_stack_size = 0
 
-        size_before_generating = self.bytecode_size
         if is_internal:
             builtin.generate_internal_opcodes(self)
         else:
             builtin.generate_opcodes(self)
-
-        if size_before_generating == self.bytecode_size:
-            # TODO: remove this when the built in code generation refactoring is finished
-            for opcode, data in builtin.opcode:
-                op_info = OpcodeInfo.get_info(opcode)
-                if opcode is Opcode.CALL and isinstance(data, Method):
-                    # avoid losing current stack state
-                    for _ in data.args:
-                        self._stack_append(Type.any)
-                    self.convert_method_call(data, len(data.args) - 1)
-                else:
-                    self.__insert1(op_info, data)
 
         if isinstance(builtin, IBuiltinMethod):
             if is_internal and hasattr(builtin, 'internal_call_args'):
@@ -1919,12 +1906,17 @@ class CodeGenerator:
         :param operation: the operation that will be converted
         :param is_internal: whether it was called when generating other implemented symbols
         """
+        stack_before = len(self._stack)
         if is_internal:
             operation.generate_internal_opcodes(self)
         else:
             operation.generate_opcodes(self)
 
-        for op in range(operation.op_on_stack):
+        expected_stack_after = stack_before - operation.op_on_stack
+        if expected_stack_after < 0:
+            expected_stack_after = 0
+
+        while expected_stack_after < len(self._stack):
             self._stack_pop()
         self._stack_append(operation.result)
 
@@ -1967,11 +1959,13 @@ class CodeGenerator:
         self._stack_pop()
         self._stack_append(Type.exception)
 
-    def convert_raise_exception(self):
-        if len(self._stack) == 0:
-            self.convert_literal(Builtin.Exception.default_message)
+    def convert_raise_exception(self, *, is_internal: bool = False):
+        if not is_internal:
+            if len(self._stack) == 0:
+                self.convert_literal(Builtin.Exception.default_message)
 
-        self._stack_pop()
+        if len(self._stack) > 0:
+            self._stack_pop()
         self.__insert1(OpcodeInfo.THROW)
 
     def insert_opcode(self, opcode: Opcode, data: bytes = None,
