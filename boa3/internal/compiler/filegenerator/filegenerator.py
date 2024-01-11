20d8e4ae67356c911c9e51bfc2c2f1736e9cb1d1
Joao Lippi <joao.p.lippi@gmail.com>
2024-01-11 17:04:36 -0300

    CU-86a1n2upk - Notify is not being registered on events when using `notify` as a exported member of another import

--- boa3/internal/compiler/filegenerator/filegenerator.py
+++ boa3/internal/compiler/filegenerator/filegenerator.py
@@ -189,7 +189,14 @@ class FileGenerator:
             events = set()
             for imported in self._all_imports:
                 events.update([event for event in imported.all_symbols.values() if isinstance(event, Event)])
-            events.update([event for event in self._symbols.values() if isinstance(event, Event)])
+
+            for symbol in self._symbols.values():
+                if isinstance(symbol, Event):
+                    events.add(symbol)
+                elif isinstance(symbol, Package):
+                    for package_symbol in symbol.symbols.values():
+                        if isinstance(package_symbol, Event):
+                            events.add(package_symbol)
 
             self._inner_events = {event.name: event for event in events}
         return self._inner_events
