cb4b5ec18ad13ad642e63421bb25bc97ea1e1736
Mirella de Medeiros <mirella.medeiros@simpli.com.br>
2024-02-20 14:44:31 -0300

    CU-86a1huqp8 - Refactor test_logger.py to use BoaTestConstructor

--- boa3_test/tests/compiler_tests/test_logger.py
+++ boa3_test/tests/compiler_tests/test_logger.py
@@ -1,7 +1,7 @@
-from boa3_test.tests.boa_test import BoaTest  # needs to be the first import to avoid circular imports
+from boa3_test.tests import boatestcase
 
 
-class TestImport(BoaTest):
+class TestImport(boatestcase.BoaTestCase):
     def test_log_multiple_errors_on_import(self):
         path = self.get_contract_path('test_sc/import_test', 'ImportFailInnerNotExistingMethod.py')
         errors, _ = self.get_all_compile_log_data(path, fail_fast=False)
