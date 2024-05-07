29098451c5e56defeda6d92d21010c30b65e12e5
luc10921 <lucas.uezu@simpli.com.br>
2024-05-07 15:48:06 -0300

    CU-86dtb6tu5 - Change the usage of `typing.Union` and `typing.Optional` to use the native `|` operator and update tests accordingly

--- boa3/internal/model/operation/binary/additional/__init__.py
+++ boa3/internal/model/operation/binary/additional/__init__.py
@@ -1,8 +1,6 @@
 __all__ = ['CollectionMembership',
            'CollectionNotMembership',
-           'UnionTypesOperation'
            ]
 
 from boa3.internal.model.operation.binary.additional.membership import CollectionMembership
 from boa3.internal.model.operation.binary.additional.notmembership import CollectionNotMembership
-from boa3.internal.model.operation.binary.additional.uniontypesoperation import UnionTypesOperation
