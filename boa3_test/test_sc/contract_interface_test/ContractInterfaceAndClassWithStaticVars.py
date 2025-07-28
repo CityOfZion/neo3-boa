4740507694e54ad2cd76327356f47c805cc2437e
Lucas Kenji Uezu <kenjiuezu3@gmail.com>
2025-07-28 10:35:56 -0300

    Fix error when classes with contract decorator are in the same file as classes with static vars

---
+++ boa3_test/test_sc/contract_interface_test/ContractInterfaceAndClassWithStaticVars.py
@@ -0,0 +1,24 @@
+from boa3.sc.compiletime import public
+
+
+@public
+def main() -> int:
+    return ExampleStaticVars.value1
+
+
+from boa3.builtin.compile_time import contract
+from boa3.builtin.type import UInt160
+
+
+@contract("0x0123456789abcdef0123456789abcdef01234567")
+class Example:
+    hash: UInt160
+
+    @staticmethod
+    def total() -> int:
+        pass
+
+
+class ExampleStaticVars:
+    value1 = 1
+    value2 = 2
