65dac66acc0f9deb137f52e4ca9abf1f30911406
Lucas Kenji Uezu <kenjiuezu3@gmail.com>
2026-01-19 11:05:39 -0300

    move imports to top of file

--- boa3/internal/model/builtin/native/stdlib/hexdecodemethod.py
+++ boa3/internal/model/builtin/native/stdlib/hexdecodemethod.py
@@ -1,11 +1,11 @@
 from boa3.internal.model.builtin.interop.nativecontract import StdLibMethod
+from boa3.internal.model.type.type import Type
 from boa3.internal.model.variable import Variable
 
 
 class HexDecodeMethod(StdLibMethod):
 
     def __init__(self):
-        from boa3.internal.model.type.type import Type
         identifier = 'hex_decode'
         native_identifier = 'hexDecode'
         args: dict[str, Variable] = {'hex_string': Variable(Type.str)}
--- boa3/internal/model/builtin/native/stdlib/hexencodemethod.py
+++ boa3/internal/model/builtin/native/stdlib/hexencodemethod.py
@@ -1,11 +1,11 @@
 from boa3.internal.model.builtin.interop.nativecontract import StdLibMethod
+from boa3.internal.model.type.type import Type
 from boa3.internal.model.variable import Variable
 
 
 class HexEncodeMethod(StdLibMethod):
 
     def __init__(self):
-        from boa3.internal.model.type.type import Type
         identifier = 'hex_encode'
         native_identifier = 'hexEncode'
         args: dict[str, Variable] = {'bytes_': Variable(Type.bytes)}
